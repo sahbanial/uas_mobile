cae36cdab006023c1d590014f892dd25bfc3a811
Houadria Hamza <hhouadria@toolynk.com>
2020-10-25 16:41:10 +0100

    oine signal config

--- src/containers/formations/index.js
+++ src/containers/formations/index.js
@@ -18,40 +18,66 @@ import Images from '@config/Images';
 import {WIDTH} from '@config/Constant';
 import {Badge} from 'react-native-paper';
 import BottomNavBar from '@components/BottomNavBar';
+import {BoxShadow} from 'react-native-shadow';
+import {useQuery} from 'react-apollo';
+import gql from 'graphql-tag';
+const shadowOpt = {
+  width: scale(330),
+  height: scale(105),
+  color: '#ddd',
+  border: 2,
+  radius: 6,
+  opacity: 0.2,
+  x: 0,
+  y: 2,
 
+  style: {marginVertical: 5},
+};
 const FormItem = ({title}) => (
-  <Box
-    flexDirection="row"
-    alignItems="center"
-    padding="s"
-    borderBottomWidth={1}
-    borderColor="gray"
-    style={styles.shadow}
-    marginTop="s">
-    <Box>
-      <Image
-        source={{
-          uri:
-            'https://77f74907-a-62cb3a1a-s-sites.googlegroups.com/site/wwwnewtechnologiecom/cisco-ccna-1-2-3-4/Cisco_CCNA_Logo512.png?attachauth=ANoY7coIy1l2XHXUQcuQ2c0CCxBuVq_nnpZFZXwFUNQHuUtn5D1f_3Ql27pIxdOwwf0Wp52BBMv24mUOuWAXlKw4WLToLOb9F6zOwX6Ar_RX3-G1aXJdTwCRAxfNubEo9eHTCeyWcJHt3O8MZLawxxaFiU5SRz1Yu_RLYI9jYuco-YYuxzJZ1_uGoa1sWwwnvkaW-rY8vJuofMXLJrIpzuj0Xnq9ZeRLXstb7vaBg_XlsciddJ3L1IO7p85HVYZ-iDy7oSaY7C8u&attredirects=0',
-        }}
-        style={{height: 70, width: 70}}
-      />
+  <BoxShadow setting={shadowOpt}>
+    <Box
+      flexDirection="row"
+      alignItems="center"
+      padding="s"
+      borderBottomWidth={1}
+      borderColor="gray"
+      style={styles.shadow}
+      margin="s">
+      <Box>
+        <Image
+          source={{
+            uri:
+              'https://77f74907-a-62cb3a1a-s-sites.googlegroups.com/site/wwwnewtechnologiecom/cisco-ccna-1-2-3-4/Cisco_CCNA_Logo512.png?attachauth=ANoY7coIy1l2XHXUQcuQ2c0CCxBuVq_nnpZFZXwFUNQHuUtn5D1f_3Ql27pIxdOwwf0Wp52BBMv24mUOuWAXlKw4WLToLOb9F6zOwX6Ar_RX3-G1aXJdTwCRAxfNubEo9eHTCeyWcJHt3O8MZLawxxaFiU5SRz1Yu_RLYI9jYuco-YYuxzJZ1_uGoa1sWwwnvkaW-rY8vJuofMXLJrIpzuj0Xnq9ZeRLXstb7vaBg_XlsciddJ3L1IO7p85HVYZ-iDy7oSaY7C8u&attredirects=0',
+          }}
+          style={{height: 70, width: 70}}
+        />
+      </Box>
+      <Box marginLeft="s" width={scale(230)}>
+        <Text color="black" variant="medium">
+          {title}
+        </Text>
+        <Text textAlign="justify" variant="small" color="gray1">
+          As Enterprises migrate toward controller based architectures, the role
+          and skills required of a core network engineer are evolving and more
+          vital than ever. To prepare for this network transition
+        </Text>
+      </Box>
     </Box>
-    <Box marginLeft="s" width={scale(230)}>
-      <Text color="black" variant="medium">
-        {title}
-      </Text>
-      <Text textAlign="justify" variant="small" color="gray1">
-        As Enterprises migrate toward controller based architectures, the role
-        and skills required of a core network engineer are evolving and more
-        vital than ever. To prepare for this network transition
-      </Text>
-    </Box>
-  </Box>
+  </BoxShadow>
 );
-
+const QUERY = gql`
+  query {
+    getFormations {
+      name
+      subTitle
+      description
+      photoUrl
+    }
+  }
+`;
 const Formations = (props) => {
   const [index, setIndex] = React.useState(0);
+  const {data, loading, ...rest} = useQuery(QUERY);
 
   return (
     <Box flex={1} backgroundColor="mainBackground">
@@ -76,9 +102,9 @@ const Formations = (props) => {
           </Text>
           <Box>
             <ScrollView>
-              <FormItem title="CCNA Routing & Switching" />
-              <FormItem title="HCNA Routing & Switching" />
-              <FormItem title="CCNA Routing & Switching" />
+              {data?.getFormations?.map((fr) => (
+                <FormItem title={fr?.name} />
+              ))}
             </ScrollView>
           </Box>
         </Box>
@@ -102,15 +128,13 @@ const styles = StyleSheet.create({
     elevation: 0,
     borderRadius: 100,
   },
-  shadow:{
-    
-      shadowOffset: { width: 100, height: 100 },
-  shadowColor: 'black',
-  shadowOpacity: 1,
-  //elevation: 0.4,
-  zIndex:9999,
-  backgroundColor:"white"
-  
-  }
+  shadow: {
+    shadowOffset: {width: 100, height: 100},
+    shadowColor: 'black',
+    shadowOpacity: 1,
+    //elevation: 0.4,
+    zIndex: 9999,
+    backgroundColor: 'white',
+  },
 });
 export default Formations;
